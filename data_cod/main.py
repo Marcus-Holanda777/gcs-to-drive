937c8bc65aa69d4752680de90d79bb7ed651c03d
Marcus Holanda <mvsh777@hotmail.com>
2024-06-21 09:52:55 -0300

    segredos

--- data_cod/main.py
+++ data_cod/main.py
@@ -6,24 +6,23 @@ import mimetypes
 import os
 from google.cloud import storage
 import traceback
+from secret import access_secret_version
+import json
 
 
-JSON_DICT = {
-  "type": os.environ['type'],
-  "project_id": os.environ['project_id'],
-  "private_key_id": os.environ['private_key_id'],
-  "private_key": os.environ['private_key'].replace('\\n', '\n'),
-  "client_email": os.environ['client_email'],
-  "client_id": os.environ['client_id'].replace(',', ''),
-  "auth_uri": os.environ['auth_uri'],
-  "token_uri": os.environ['token_uri'],
-  "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
-  "client_x509_cert_url": os.environ['client_x509_cert_url'],
-  "universe_domain": os.environ['universe_domain']
-}
-
+SECRET_ID = os.environ['secret_id']
+PROJECT_IC = os.environ['project_id']
 PATH_UC = os.environ['id_path']
 SCOPES=["https://www.googleapis.com/auth/drive"]
+
+
+JSON_DICT = json.loads(
+    access_secret_version(
+        project_id=PROJECT_IC,
+        secret_id=SECRET_ID,
+        version_id=1
+    )
+)
 
 
 def download_gcs(
